c33b88184644327ba640b86e7fee8405f6c16680
salohiddin.m <s.muhiddinov@ssdsoftwaresolutions.com>
2024-01-11 00:11:16 +0500

    try to fix deplot

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3006",
+        "BASE_URL": "https://tuit-project-bf2f64.netlify.app",
         "MONGODB_URL": "mongodb+srv://fenixfire5454:Ihy4womyUCEEtHnM@cluster0.fq31og8.mongodb.net/tuit?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
-        "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
-        "CLOUD_UPDATE_PRESET": "YOUR_CLOUD_UPDATE_PRESET",
-        "CLOUD_NAME": "YOUR_CLOUD_NAME",
-        "CLOUD_API": "YOUR_CLOUD_API"
     }
 }
